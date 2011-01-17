b0ad162632035c2662387d0bdbc2a3fd8a99e147
Iguana42 <briancicutti@msn.com>
2011-01-17 14:36:30 -0800

    Search added to recipes model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,5 +6,13 @@ class Recipe < ActiveRecord::Base
   accepts_nested_attributes_for :ingredients, :allow_destroy => true
   
   scope :where_title, lambda { |term| where("recipes.title LIKE ?", "%#{term}%")}
+
+    def self.search(search)
+      if search      
+        find(:all, :conditions => ['tag LIKE ?', "%#{search}%"])
+      else
+        find(:all)
+      end
+    end  
 end                       
 
